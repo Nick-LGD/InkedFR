842c4f224aceb44757b78f6431bf8520aec81837
Nicolas <n.legrand77190@gmail.com>
2020-12-20 18:08:15 +0100

    fix

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -17,7 +17,6 @@ use App\Repository\HomepageRepository;
 use App\Repository\MoreRepository;
 use DateTime;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
